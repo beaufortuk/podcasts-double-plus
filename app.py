fb4e84c0644989110472a39616a10c3e1fd2547c
beaufortuk <sellotape@gmail.com>
2024-10-09 18:10:24 +0100

    Fun witih dependencies and AWS EB

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ def understand_more():
     return render_template('results.html', episodes=recommended_episodes, type='Understand More')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
